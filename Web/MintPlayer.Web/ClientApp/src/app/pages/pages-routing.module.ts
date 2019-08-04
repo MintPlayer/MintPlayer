50043f89aad22a85735b2089f49d38c0360e942e
MusicDemons <pieterjandeclippel@hotmail.com>
2019-08-04 23:03:44 +0200

    AccountModule

--- Web/MintPlayer.Web/ClientApp/src/app/pages/pages-routing.module.ts
+++ Web/MintPlayer.Web/ClientApp/src/app/pages/pages-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'account', loadChildren: './account/account.module#AccountModule' }
 ];
 
 @NgModule({
--- Web/MintPlayer.Web/ClientApp/src/app/pages/pages.module.ts
+++ Web/MintPlayer.Web/ClientApp/src/app/pages/pages.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AccountModule } from './account/account.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    AccountModule
   ]
 })
 export class PagesModule { }
