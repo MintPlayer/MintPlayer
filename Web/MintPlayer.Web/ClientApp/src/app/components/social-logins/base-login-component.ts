fe248c95ca52a5d49cbf3ed71f5348e200f750ce
Pieterjan De Clippel <pieterjandeclippel@hotmail.com>
2019-08-13 14:18:32 +0200

    Refactor Facebook Login

---
+++ Web/MintPlayer.Web/ClientApp/src/app/components/social-logins/base-login-component.ts
@@ -0,0 +1,57 @@
+import { Input, Output, EventEmitter } from '@angular/core';
+import { LoginResult } from '../../interfaces/loginResult';
+
+export class BaseLoginComponent {
+  protected authWindow: Window;
+  protected listener: any;
+  public isOpen: boolean = false;
+
+  @Input() public action: "add" | "connect";
+  @Output() public LoginSuccessOrFailed: EventEmitter<LoginResult> = new EventEmitter();
+
+  constructor(protected baseUrl: string, protected platform: string) {
+    this.listener = this.handleMessage.bind(this);
+    if (window.addEventListener) {
+      window.addEventListener("message", this.listener, false);
+    } else {
+      (<any>window).attachEvent("onmessage", this.listener);
+    }
+  }
+
+  protected dispose() {
+    if (window.removeEventListener) {
+      window.removeEventListener("message", this.listener, false);
+    } else {
+      (<any>window).detachEvent("onmessage", this.listener);
+    }
+  }
+
+  showPopup() {
+    this.authWindow = window.open(`${this.baseUrl}/api/Account/${this.action}/${this.platform}`, null, 'width=600,height=400');
+    this.isOpen = true;
+    var timer = setInterval(() => {
+      if (this.authWindow.closed) {
+        this.isOpen = false;
+        clearInterval(timer);
+      }
+    });
+  }
+
+  private handleMessage(event: Event) {
+    const message = event as MessageEvent;
+    // Only trust messages from the below origin.
+    if (!this.baseUrl.startsWith(message.origin)) return;
+
+    // Filter out Augury
+    if (message.data.messageSource != null)
+      if (message.data.messageSource.indexOf("AUGURY_") > -1) return;
+    // Filter out any other trash
+    if (message.data == "" || message.data === null || message.data === undefined) return;
+
+    const result = <LoginResult>JSON.parse(message.data);
+    if (result.platform == this.platform) {
+      this.authWindow.close();
+      this.LoginSuccessOrFailed.emit(result);
+    }
+  }
+}
--- Web/MintPlayer.Web/ClientApp/src/app/components/social-logins/facebook-login/facebook-login.component.ts
+++ Web/MintPlayer.Web/ClientApp/src/app/components/social-logins/facebook-login/facebook-login.component.ts
@@ -1,68 +1,20 @@
-import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
-import { LoginResult } from '../../../interfaces/loginResult';
+import { Component, OnInit, Inject } from '@angular/core';
+import { BaseLoginComponent } from '../base-login-component';
 
 @Component({
   selector: 'app-facebook-login',
   templateUrl: './facebook-login.component.html',
   styleUrls: ['./facebook-login.component.scss']
 })
-export class FacebookLoginComponent implements OnInit {
-
-  private authWindow: Window;
-  public isOpen: boolean = false;
-
-  @Input() public action: "add" | "connect";
-
-  @Output() public LoginSuccessOrFailed: EventEmitter<LoginResult> = new EventEmitter();
-
-  launchFbLogin() {
-    this.authWindow = window.open(`${this.baseUrl}/api/Account/${this.action}/Facebook`, null, 'width=600,height=400');
-    this.isOpen = true;
-    var timer = setInterval(() => {
-      if (this.authWindow.closed) {
-        this.isOpen = false;
-        clearInterval(timer);
-      }
-    });
-  }
-
-  private listener: any;
-  constructor(@Inject('BASE_URL') private baseUrl: string) {
-    this.listener = this.handleMessage.bind(this);
-    if (window.addEventListener) {
-      window.addEventListener("message", this.listener, false);
-    } else {
-      (<any>window).attachEvent("onmessage", this.listener);
-    }
+export class FacebookLoginComponent extends BaseLoginComponent implements OnInit {
+  constructor(@Inject('BASE_URL') baseUrl: string) {
+    super(baseUrl, 'Facebook');
   }
 
   ngOnInit() {
   }
 
   ngOnDestroy() {
-    if (window.removeEventListener) {
-      window.removeEventListener("message", this.listener, false);
-    } else {
-      (<any>window).detachEvent("onmessage", this.listener);
-    }
-  }
-
-  handleMessage(event: Event) {
-    const message = event as MessageEvent;
-    // Only trust messages from the below origin.
-    if (!this.baseUrl.startsWith(message.origin)) return;
-
-    // Filter out Augury
-    if (message.data.messageSource != null)
-      if (message.data.messageSource.indexOf("AUGURY_") > -1) return;
-    // Filter out any other trash
-    if (message.data == "" || message.data === null || message.data === undefined) return;
-
-    debugger;
-    const result = <LoginResult>JSON.parse(message.data);
-    if (result.platform == "Facebook") {
-      this.authWindow.close();
-      this.LoginSuccessOrFailed.emit(result);
-    }
+    this.dispose();
   }
 }
