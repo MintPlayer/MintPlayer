9f1bcd7efbaec95d6f0bbfc1c8ca26478cac78f0
MusicDemons <pieterjandeclippel@hotmail.com>
2019-08-19 22:33:48 +0200

    Added Service

---
+++ Crawler/MintPlayerCrawler.Service/MintPlayerCrawlerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace MintPlayerCrawler.Service
+{
+    public class MintPlayerCrawlerService : ServiceBase
+    {
+        public MintPlayerCrawlerService()
+        {
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+            Console.WriteLine("Started MintPlayer crawler service");
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Console.WriteLine("Stopped MintPlayer crawler service");
+        }
+    }
+}
---
+++ Crawler/MintPlayerCrawler.Service/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace MintPlayerCrawler.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Any())
+                {
+                    switch (args[0])
+                    {
+                        case "install":
+                            break;
+                        case "sayhello":
+                            Console.WriteLine("Hello World!");
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine();
+                Console.WriteLine("  Usage:");
+                Console.WriteLine("  MintPlayerCrawler.Service.exe sayhello");
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+        }
+    }
+}
