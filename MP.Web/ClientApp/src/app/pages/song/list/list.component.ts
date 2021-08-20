7a13d0759ca81297bdae544440c87e5768f6390e
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2021-08-20 13:36:44 +0200

    Removed console.log

--- MP.Web/ClientApp/src/app/pages/song/list/list.component.ts
+++ MP.Web/ClientApp/src/app/pages/song/list/list.component.ts
@@ -94,7 +94,6 @@ export class ListComponent implements OnInit, OnDestroy {
   }
 
   private setSongData(data: PaginationResponse<Song>) {
-    console.error('song data', data);
     this.songData = data;
     this.tableSettings.page.values = Array.from(Array(data.totalPages).keys()).map((p) => p + 1);
   }
