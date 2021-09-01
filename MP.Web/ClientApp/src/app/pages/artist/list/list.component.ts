e7f20b8f7e9ac78672d6c55fb4a533b11559e4a2
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2021-09-01 21:46:06 +0200

    Artist-list - paging queryparameters

--- MP.Web/ClientApp/src/app/pages/artist/list/list.component.ts
+++ MP.Web/ClientApp/src/app/pages/artist/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
-import { UrlSerializer } from '@angular/router';
+import { ActivatedRoute, UrlSerializer } from '@angular/router';
 import { Title, Meta } from '@angular/platform-browser';
 import { AdvancedRouter } from '@mintplayer/ng-router';
 import { DatatableSettings } from '@mintplayer/ng-datatables';
@@ -7,6 +7,8 @@ import { SERVER_SIDE } from '@mintplayer/ng-server-side';
 import { PaginationResponse } from '@mintplayer/ng-pagination';
 import { Artist, ArtistService } from '@mintplayer/ng-client';
 import { HtmlLinkHelper } from '../../../helpers/html-link.helper';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
@@ -20,6 +22,7 @@ export class ListComponent implements OnInit, OnDestroy {
     @Inject('ARTISTS') private artistsInj: PaginationResponse<Artist>,
     private artistService: ArtistService,
     private router: AdvancedRouter,
+    private route: ActivatedRoute,
     private urlSerializer: UrlSerializer,
     private titleService: Title,
     private htmlLink: HtmlLinkHelper,
@@ -31,6 +34,21 @@ export class ListComponent implements OnInit, OnDestroy {
     } else {
       this.loadArtists();
     }
+
+    this.route.queryParams
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((queryParams) => {
+        this.tableSettings.perPage.selected = parseInt(queryParams['perpage'] ?? 20);
+        this.tableSettings.page.selected = parseInt(queryParams['page'] ?? 1);
+        this.tableSettings.sortProperty = queryParams['sortproperty'] ?? 'name';
+        this.tableSettings.sortDirection = queryParams['sortdirection'] ?? 'ascending';
+
+        this.artistService.pageArtists(this.tableSettings.toPagination()).then((response) => {
+          this.setArtistData(response);
+        }).catch((error) => {
+          console.error(error);
+        });
+      })
   }
 
   ngOnInit() {
@@ -38,9 +56,11 @@ export class ListComponent implements OnInit, OnDestroy {
     this.addMetaTags();
   }
 
+  private destroyed$ = new Subject();
   ngOnDestroy() {
     this.htmlLink.unset('canonical');
     this.removeMetaTags();
+    this.destroyed$.next(true);
   }
 
   //#region Add meta-tags
@@ -84,10 +104,13 @@ export class ListComponent implements OnInit, OnDestroy {
   //#endregion
 
   loadArtists() {
-    this.artistService.pageArtists(this.tableSettings.toPagination()).then((response) => {
-      this.setArtistData(response);
-    }).catch((error) => {
-      console.error(error);
+    this.router.navigate([], {
+      queryParams: {
+        perpage: this.tableSettings.perPage.selected,
+        page: this.tableSettings.page.selected,
+        sortproperty: this.tableSettings.sortProperty,
+        sortdirection: this.tableSettings.sortDirection,
+      }
     });
   }
 
