8b8eb2a22798bd9e272969f6a05b1fa66a435872
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2021-09-18 11:59:50 +0200

    Genius song v2 media

---
+++ Fetcher/Genius/MintPlayer.Fetcher.Genius/Parsers/V2/Data/MediumData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MintPlayer.Fetcher.Genius.Parsers.V2.Data
+{
+	internal class MediumData
+	{
+		[JsonProperty("url")]
+		public string Url { get; set; }
+
+		[JsonProperty("type")]
+		public string Type { get; set; }
+
+		[JsonProperty("provider")]
+		public string Provider { get; set; }
+	}
+}
--- Fetcher/Genius/MintPlayer.Fetcher.Genius/Parsers/V2/Mappers/SongV2Mapper.cs
+++ Fetcher/Genius/MintPlayer.Fetcher.Genius/Parsers/V2/Mappers/SongV2Mapper.cs
@@ -18,11 +18,26 @@ namespace MintPlayer.Fetcher.Genius.Parsers.V2.Mappers
 
 			var primaryArtistId = Convert.ToInt64(songData.SongPage.TrackingData.First(td => td.Key == "Primary Artist ID").Value);
 			var releaseDate = (string)songData.SongPage.TrackingData.First(td => td.Key == "Release Date").Value;
+			var media = songData.Entities.SelectToken($"songs.{songData.SongPage.Id}.media").ToObject<Data.MediumData[]>()
+				.Select(m =>
+				{
+					switch (m.Provider)
+					{
+						case "spotify":
+							return new Medium { Type = MintPlayer.Fetcher.Abstractions.Enums.eMediumType.Spotify, Value = m.Url };
+						case "soundcloud":
+							return new Medium { Type = MintPlayer.Fetcher.Abstractions.Enums.eMediumType.SoundCloud, Value = m.Url };
+						case "youtube":
+							return new Medium { Type = MintPlayer.Fetcher.Abstractions.Enums.eMediumType.YouTube, Value = m.Url };
+						default:
+							return null;
+					}
+				}).Where(m => m != null);
 
 			var result = new Song
 			{
 				Id = songData.SongPage.Id,
-				Url = $"https://genius.com${songData.SongPage.Url}",
+				Url = $"https://genius.com{songData.SongPage.Url}",
 				Title = (string)songData.SongPage.TrackingData.First(td => td.Key == "Title").Value,
 				ReleaseDate = DateTime.Parse(releaseDate),
 				PrimaryArtist = new Artist
@@ -30,7 +45,8 @@ namespace MintPlayer.Fetcher.Genius.Parsers.V2.Mappers
 					Id = primaryArtistId,
 					Name = (string)songData.Entities.SelectToken($"artists.{primaryArtistId}.name"),
 					Url = (string)songData.Entities.SelectToken($"artists.{primaryArtistId}.url"),
-				}
+				},
+				Media = media.ToList()
 			};
 
 			return Task.FromResult(result);
