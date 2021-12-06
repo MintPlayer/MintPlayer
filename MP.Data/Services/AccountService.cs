0396c854079dd79dc644504f0706786745eb5d9f
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2021-12-06 14:33:21 +0100

    Moved IAccountService

--- MP.Data/Services/AccountService.cs
+++ MP.Data/Services/AccountService.cs
@@ -7,12 +7,45 @@ using Microsoft.AspNetCore.Authentication;
 using MintPlayer.Dtos.Dtos;
 using MintPlayer.Data.Repositories;
 using System.Net.Mail;
+using Microsoft.Extensions.Options;
+using MintPlayer.Data.Options;
+using MintPlayer.Data.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using MintPlayer.Data.Abstractions.Services;
 
 namespace MintPlayer.Data.Services
 {
+	public interface IAccountService
+	{
+		Task Register(User user, string password);
+		Task SendConfirmationEmail(string email);
+		Task VerifyEmailConfirmationToken(string email, string token);
+		Task<string> GeneratePasswordResetToken(string email);
+		Task SendPasswordResetEmail(string email, string resetUrl);
+		Task ResetPassword(string email, string token, string newPassword);
+		Task<LocalLoginResult> LocalLogin(string email, string password, bool createCookie);
+		Task<IEnumerable<string>> GetProviders();
+		Task<AuthenticationProperties> ConfigureExternalAuthenticationProperties(string provider, string redirectUrl);
+		Task<ExternalLoginResult> PerfromExternalLogin();
+		Task<IEnumerable<UserLoginInfo>> GetExternalLogins(ClaimsPrincipal userProperty);
+		Task AddExternalLogin(ClaimsPrincipal userProperty);
+		Task RemoveExternalLogin(ClaimsPrincipal userProperty, string provider);
+		Task<User> GetCurrentUser(ClaimsPrincipal userProperty);
+		Task<bool> GetHasPassword(ClaimsPrincipal userProperty);
+		Task UpdatePassword(ClaimsPrincipal userProperty, string currentPassword, string newPassword, string confirmation);
+		Task<IEnumerable<string>> GetCurrentRoles(ClaimsPrincipal userProperty);
+		Task Logout();
+
+		Task<string> GenerateTwoFactorRegistrationCode(ClaimsPrincipal userProperty);
+		Task<IEnumerable<string>> GenerateTwoFactorBackupCodes(ClaimsPrincipal userProperty);
+		Task SetTwoFactorEnabled(ClaimsPrincipal userProperty, string code, bool enable);
+		Task SetTwoFactorBypass(ClaimsPrincipal userProperty, bool bypass, string code);
+		Task<User> TwoFactorLogin(string authenticatorCode, bool remember);
+		Task TwoFactorRecovery(string backupCode);
+		Task<int> GetRemainingNumberOfRecoveryCodes(ClaimsPrincipal userProperty);
+	}
 	internal class AccountService : IAccountService
     {
         private readonly IAccountRepository accountRepository;
